1c3a543e0c81a1f8432cf2f035dd3b826689f697
CropData Cloud <100765014+cloud-cropdata-in@users.noreply.github.com>
2022-10-16 12:48:25 +0530

    updated the error for cc email

--- mail_service/functions.py
+++ mail_service/functions.py
@@ -1,27 +1,3 @@
-# from sendgrid import SendGridAPIClient
-# from sendgrid.helpers.mail import *
-
-
-# def sendGridMail(from_email, to_emails, cc_emails, subject, body, api_key):
-#     """Sendgrid  mail service method"""
-#
-#     personalization = Personalization()
-#     for email in to_emails:
-#         personalization.add_to(Email(email))
-#     for email in cc_emails:
-#         personalization.add_cc(Email(email))
-#     message = Mail(
-#         from_email=Email(from_email),
-#         subject=subject,
-#         html_content=body)
-#     message.add_personalization(personalization)
-#     try:
-#         sendgrid_client = SendGridAPIClient(api_key=api_key)
-#         sendgrid_client.send(message)
-#     except Exception as e:
-#         print(e)
-
-
 import smtplib
 import ssl
 from email.mime.multipart import MIMEMultipart
@@ -35,7 +11,8 @@ def smtpMail(sender_email, receiver_email, password, message, subject=None, cc_e
     msg["Subject"] = subject
     msg["From"] = sender_email
     msg["To"] = ', '.join(receiver_email)
-    msg["Cc"] = ', '.join(cc_email)
+    if cc_email:
+        msg["Cc"] = ', '.join(cc_email)
     part2 = MIMEText(message, 'html')
     msg.attach(part2)
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mail-service',
-    version='0.7.0',
+    version='1.0.0',
     author='Rishabh Oswal',
     author_email='rishabh.oswal@cropdata.in',
     description='Mail Service Package',
