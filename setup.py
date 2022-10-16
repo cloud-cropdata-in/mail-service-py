0a1886e55fc4f5b90d6b18f6f5f651898a9c5094
CropData Cloud <100765014+cloud-cropdata-in@users.noreply.github.com>
2022-10-16 12:55:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mail-service-py',
-    version='1.0.0',
+    version='0.0.1',
     author='Rishabh Oswal',
     author_email='rishabh.oswal@cropdata.in',
     description='Mail Service Package',
